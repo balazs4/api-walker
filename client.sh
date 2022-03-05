b88fd0cc16d7719aa22226ffac31f939a0765724
balazs4 <balazs4@users.noreply.github.com>
2022-03-05 15:04:23 +0100

    replace regex with recursive fn
    regex might be faster tho

--- client.sh
+++ client.sh
@@ -5,10 +5,7 @@ baseurl=$1
 selected=true
 while [[ $selected ]]
 do
-  selected=`cat Test-TD.json \
-    # | fx 'x => { const {links = [], ...content} = x; return [JSON.stringify(content), ...links].join("\n"); }' \
-    | fx findNestedHrefs \
-    | fzf --select-1 --sync --reverse --preview 'echo {} | fx . 2>/dev/null || echo {}'`
+  selected=`fx Test-TD.json findNestedHrefs | fzf --select-1 --sync --reverse --preview 'echo {} | fx . 2>/dev/null || echo {}'`
   echo $selected | fx . 2> /dev/null && break;
   url=$baseurl$selected
 done
